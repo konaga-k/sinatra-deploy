d27ae6974483bff9dd646b3d9f55e8880b40a20d
konaga-k <priests2020@gmail.com>
2021-12-13 18:28:56 +0900

    EC2でbundle installに失敗するので最小構成に変更

--- Gemfile
+++ Gemfile
@@ -4,5 +4,3 @@ source 'https://rubygems.org'
 
 # Sinatra
 gem 'sinatra', '~> 2.1.0'
-gem 'sinatra-contrib', '~> 2.1.0'
-gem 'thin', '~> 1.8.0'
--- setup.sh
+++
@@ -1,7 +0,0 @@
-bundle install
-
-yarn install
-npx tailwindcss build ./public/css/style.css -o ./public/css/output.css
-
-cp ./data/memo.yml.sample ./data/memo.yml
-cp ./data/memo_sequence.sample ./data/memo_sequence
